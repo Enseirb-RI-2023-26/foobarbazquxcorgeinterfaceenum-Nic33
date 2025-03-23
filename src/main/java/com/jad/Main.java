c897ebd5e3ac8b58d42fedc6380308ec87a89bb8
Nicolas Dubuisson <nicolas.dubuisson@etu.u-bordeaux.fr>
2025-03-23 15:52:23 +0100

    First commit

---
+++ src/main/java/com/jad/Main.java
@@ -0,0 +1,11 @@
+package com.jad;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Création des objets
+        IBaz baz = EBaz.BAZ1;  // Utilisation de l'énumération
+
+
+    }
+}
