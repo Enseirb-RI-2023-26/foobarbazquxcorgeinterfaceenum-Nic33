fb63c1c639c6a1e17a9f5062b3cd75662aa54c0a
Nicolas Dubuisson <nicolas.dubuisson@etu.u-bordeaux.fr>
2025-03-25 11:24:23 +0100

    test ICorge

--- src/main/java/com/jad/ICorge.java
+++ src/main/java/com/jad/ICorge.java
@@ -2,4 +2,5 @@ package com.jad;
 
 public interface ICorge {
     IFoo getFoo();
+    void setFoo(IFoo foo);
 }
--- src/main/java/com/jad/Main.java
+++ src/main/java/com/jad/Main.java
@@ -1,7 +1,5 @@
 package com.jad;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
 
